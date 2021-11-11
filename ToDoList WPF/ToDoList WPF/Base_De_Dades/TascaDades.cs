46725e01902b2137bc0de4b187211e7d236f611b
IsaacDAM <85341459+IsaacDAM@users.noreply.github.com>
2021-11-11 16:21:22 +0100

    Gets i Sets Base de Dades

---
+++ ToDoList WPF/ToDoList WPF/Base_De_Dades/TascaDades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList_WPF
+{
+    class TascaDades
+    {
+        public int Codi_Tasc { get; set; }
+        public string Titol { get; set; }
+        public string Descripcio { get; set; }
+        public DateTime dCreacio { get; set; }
+        public DateTime dFinalitzacio { get; set; }
+        public enum _Prioritat { Alta, Mitja, Baixa }
+        public _Prioritat Prioritat { get; set; }
+        public TreballadorDades Representant { get; set; }
+        public enum _Estat { ToDo, Doing, Done}
+        public _Estat Estat { get; set; }
+    }
+}
---
+++ ToDoList WPF/ToDoList WPF/Base_De_Dades/TreballadorDades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList_WPF
+{
+    class TreballadorDades
+    {
+        public int Codi_Treb { get; set; }
+        public string NIF { get; set; }
+        public string Nom { get; set; }
+        public string Cognoms { get; set; }
+        public string Telefon { get; set; }
+        public string Correu { get; set; }
+    }
+}
