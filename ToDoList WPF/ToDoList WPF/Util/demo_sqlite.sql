0720e2f66bc0eb5ad27868a8c2f3cef184eb55c9
adriatorondelDAM12021 <atorondel.smx@institutcampalans.net>
2021-11-11 17:45:53 +0100

    Edit sql

--- ToDoList WPF/ToDoList WPF/Util/demo_sqlite.sql
+++ ToDoList WPF/ToDoList WPF/Util/demo_sqlite.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS treballador(
-            nom TEXT CHECK(LENGTH(nom)>20) PRIMARY KEY,
-            NIF TEXT CHECK(LENGTH(NIF)>10) NOT NULL,
-            cognoms TEXT CHECK(LENGTH(cognoms)>40),
-            tel TEXT CHECK(LENGTH(tel)>15),
-            correu TEXT CHECK(LENGTH(correu)>30)
+            NIF VARCHAR(9) NOT NULL PRIMARY KEY,
+            nom VARCHAR(15) NOT NULL,
+            cognoms VARCHAR(40),
+            tel VARCHAR(9),
+            correu VARCHAR(50)
                       )
 
-CREATE TABLE  IF NOT EXISTS tasca(
-            codi INTEGER PRIMARY KEY AUTOINCREMENT,
-            titol TEXT CHECK( LENGTH(titol)>30) NOT NULL,
-            descripcio TEXT CHECK( LENGTH(descripcio)>150),
+CREATE TABLE IF NOT EXISTS tasca(
+            codi INT PRIMARY KEY AUTOINCREMENT,
+            titol VARCHAR(20) NOT NULL,
+            descripcio VARCHAR(150),
             dCreacio DATETIME,
             dFinalitz DATETIME,
-            prioritat TEXT CHECK(prioritat IN ("Alta","Mitja","Baixa")),
-            representant TEXT,
-            estat TEXT CHECK(estat IN ("ToDo","Doing","Done")),
-            FOREIGN KEY (representant) REFERENCES nom (treballador)
+            prioritat ENUM("Alta","Mitja","Baixa"),
+            representant VARCHAR(9) ,
+            estat ENUM("ToDo","Doing","Done"),
+            FOREIGN KEY (representant) REFERENCES NIF (treballador)
           )
