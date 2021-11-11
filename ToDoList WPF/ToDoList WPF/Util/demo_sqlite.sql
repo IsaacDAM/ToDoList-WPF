9f444d831c7eb10b4cd787c4c0d8a3231db6cfa7
Guillem Carrión <gcarrion.dam@institutcampalans.net>
2021-11-11 17:06:04 +0100

    afegit sql amb dues taules

---
+++ ToDoList WPF/ToDoList WPF/Util/demo_sqlite.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS treballador(
+            nom TEXT CHECK(LENGTH(nom)>20) PRIMARY KEY,
+            NIF TEXT CHECK(LENGTH(NIF)>10) NOT NULL,
+            cognoms TEXT CHECK(LENGTH(cognoms)>40),
+            tel TEXT CHECK(LENGTH(tel)>15),
+            correu TEXT CHECK(LENGTH(correu)>30)
+                      )
+
+CREATE TABLE  IF NOT EXISTS tasca(
+            codi INTEGER PRIMARY KEY,
+            titol TEXT CHECK( LENGTH(titol)>30) NOT NULL,
+            descripcio TEXT CHECK( LENGTH(descripcio)>150),
+            dCreacio DATETIME,
+            dFinalitz DATETIME,
+            prioritat TEXT CHECK(prioritat IN ("Alta","Mitja","Baixa")),
+            representant TEXT,
+            estat TEXT CHECK(estat IN ("ToDo","Doing","Done")),
+            FOREIGN KEY (representant) REFERENCES nom (treballador)
+          )
