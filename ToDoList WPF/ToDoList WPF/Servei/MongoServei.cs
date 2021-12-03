f6ce84e4d881c97ac90b0aa4fc86e96dfa352c1b
Adrià Torondel <atorondel.dam@institutcampalans.net>
2021-12-03 17:40:11 +0100

    Create MongoServei.cs

---
+++ ToDoList WPF/ToDoList WPF/Servei/MongoServei.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using ToDoList_WPF.Entitats;
+
+namespace ToDoList_WPF.Servei
+{
+    class MongoServei
+    {
+        private MongoClient mongoClient;
+        private IMongoCollection<TascaDades> tascaCollection;
+        private IMongoCollection<TreballadorDades> treballadorCollection;
+        public MongoServei(string collection)
+        {
+            mongoClient = new MongoClient("mongodb+srv://ToDoList:tdlpwd@cluster2021.pqnkq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            var database = mongoClient.GetDatabase("ToDoList");
+            if (collection == "Treballador")
+            {
+                treballadorCollection = database.GetCollection<TreballadorDades>(collection);
+                tascaCollection = null;
+            }
+            else
+            {
+                tascaCollection = database.GetCollection<TascaDades>(collection);
+                treballadorCollection = null;
+            }
+        }
+
+    }
+}
