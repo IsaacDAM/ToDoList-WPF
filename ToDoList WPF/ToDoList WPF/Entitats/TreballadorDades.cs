cfc074dd49785b3d18638a1d2765cc560c7cac61
Guillem Carrión <gcarrion.dam@institutcampalans.net>
2021-12-03 17:39:37 +0100

    Modificades les entitats perque siguin aptes al MongoDB

--- ToDoList WPF/ToDoList WPF/Entitats/TreballadorDades.cs
+++ ToDoList WPF/ToDoList WPF/Entitats/TreballadorDades.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using MongoDB;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace ToDoList_WPF.Entitats
 {
     public class TreballadorDades
     {
+
+        [BsonId]
+        public ObjectId CodiT { get; set; }
+
+        [BsonElement("nif")]
         public String NIF { get; set; }
+
+        [BsonElement("nom")]
         public String Nom { get; set; }
+
+        [BsonElement("cognoms")]
         public String Cognoms { get; set; }
+
+        [BsonElement("telefon")]
         public String Telefon { get; set; }
+
+        [BsonElement("correu")]
         public String Correu { get; set; }
+
+
+        public TreballadorDades(string nif, string nom, string cognoms, string telefon, string correu)
+        {
+            this.NIF = nif;
+            this.Nom = nom;
+            this.Cognoms = cognoms;
+            this.Telefon = telefon;
+            this.Correu = correu;
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format ("id: {0}\nNIF: {1}\nnom: {2}\ncognoms: {3}\ntelefon: {4}\ncorreu: {5}\n", this.CodiT, this.NIF, this.Nom, this.Cognoms, this.Telefon, this.Correu);
+        }
+
     }
 }
