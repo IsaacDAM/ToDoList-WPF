ae37d1a6a13e727ab03814cfa8f9ec48e5507779
IsaacDAM <85341459+IsaacDAM@users.noreply.github.com>
2022-01-20 17:26:22 +0100

    Netejar Codi

--- ToDoList WPF/ToDoList WPF/API/TascaAPI.cs
+++ ToDoList WPF/ToDoList WPF/API/TascaAPI.cs
@@ -88,13 +88,11 @@ namespace ToDoList_WPF.API
         public async Task<TascaDades> GetTascaAsync(string Id)
         {
             TascaDades tasca = new TascaDades();
-
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri(BaseUri);
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
                 //Enviem petició GET a /tasca/(Titol de la tasca).
                 HttpResponseMessage response = await client.GetAsync($"tasca/{Id}");
                 if (response.IsSuccessStatusCode)
